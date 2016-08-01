96decd66424f5aadc2d3cb52ca698af328f5adec
HP <krist.ng@corous360.com>
2016-08-02 00:19:04 +0800

    change

--- app/src/main/java/com/example/questdot/collapsingtoolbarexample/MainActivity.java
+++ app/src/main/java/com/example/questdot/collapsingtoolbarexample/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.questdot.collapsingtoolbarexample;
 
 import android.content.Context;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -19,20 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
-        // Define a new Adapter
-        // First parameter - Context
-        // Second parameter - Layout for the row
-        // Third parameter - ID of the TextView to which the data is written
-        // Forth - the Array of data
-
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         rv = (RecyclerView)findViewById(R.id.my_recycler_view);
 
@@ -40,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         rv.setLayoutManager(new LinearLayoutManager(this));
 
         rv.setAdapter(new MyAdapter());
+
+
 
     }
 }
